c547d9332b7910226037e2a01aebaf15a1f079c4
JoseQuevedo <josequevedoegana@gmail.com>
2025-07-16 14:43:46 -0400

    last commit

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,4 @@
-const { agregarPost, obtenerPost } = require("./post");
+const { agregarPost, obtenerPost } = require("./posts");
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -6,12 +6,12 @@ app.use(cors());
 app.use(express.json());
 
 app.listen(3000, console.log("SERVIDOR ENCENDIDO"));
-app.get("/post", async (req, res) => {
+app.get("/posts", async (req, res) => {
   const post = await obtenerPost();
   res.json(post);
 });
 
-app.post("/post", async (req, res) => {
+app.post("/posts", async (req, res) => {
   const { titulo, img, descripcion } = req.body;
   await agregarPost(titulo, img, descripcion);
   res.send("Post agregado con éxito");
